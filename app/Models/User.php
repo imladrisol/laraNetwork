076a370cac81791fa2b84ec515118be132bc61b6
imladris <imladrisol@gmail.com>
2015-10-30 15:25:24 +0200

    Friend model methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -94,4 +94,28 @@ class User extends Model implements AuthenticatableContract/*,
     public function friendRequests(){
         return $this->friendsOfMine()->wherePivot('accepted', false)->get();
     }
+
+    public function friendRequestsPendig(){
+        return $this->friendOf()->wherePivot('accepted', false)->get();
+    }
+    public function hasFriendRequestPending(User $user){
+        return (bool) $this->friendRequestsPendig()->where('id', $user->id)->count();
+    }
+
+    public function hasFriendRequestReceived(User $user){
+        return (bool) $this->friendRequests()->where('id', $user->id)->count();
+    }
+
+    public function addFriend(User $user){
+        $this->friendOf()->attach($user->id);
+    }
+    public function acceptFriendRequest(User $user){
+        $this->friendRequests()->where('id', $user->id)->first()->pivot->update([
+            'accepted' => true,
+        ]);
+    }
+
+    public function isFriendsWith(User $user){
+        return (bool) $this->friends()->where('id', $user->id)->count();
+    }
 }
